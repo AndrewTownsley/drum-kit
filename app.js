94948f71d5d782275f49903ed16902b2e9bea77c
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-05 22:54:37 -0600

    variables declared, event listener started

---
+++ app.js
@@ -0,0 +1,14 @@
+// When the key that is on the button is pressed, the corresponding sound is played.
+// When the key is pressed, the button also plays the animation.
+
+// Declare variables...Keys, Audio
+
+const key = document.querySelectorAll('.key');
+
+function keyPress(e) {
+// Detect whick key is pressed
+
+}
+
+// addEventListener for keydown on specific key
+key.addEventListener('keydown', keyPress(e))
