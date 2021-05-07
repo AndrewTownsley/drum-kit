076e09accf07314fee8db65f0c4ec563e9993665
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-06 21:09:31 -0600

    drum-kit

--- app.js
+++ app.js
@@ -3,12 +3,21 @@
 
 // Declare variables...Keys, Audio
 
-const key = document.querySelectorAll('.key');
+// addEventListener for keydown on specific key
+window.addEventListener('keydown', keyPress)
 
 function keyPress(e) {
 // Detect whick key is pressed
+// console.log(e.keyCode);
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// Check if there is an audio element for the key that was pressed
+// If there is not, then stlp this function from running until another key is pressed.
+if(!audio) {return};
+audio.play();
+// Play the corresponding audio of the pressed key.
+
+// add the class of 'playing' to the key div of the key that is being pressed.
+// key.classList.add('playing');
 
 }
 
-// addEventListener for keydown on specific key
-key.addEventListener('keydown', keyPress(e))
