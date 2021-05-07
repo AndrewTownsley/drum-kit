b8d4227c1541ad9d69dd7cf5faaa99f2818b929a
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-06 23:19:37 -0600

    app finished, except that the animation class on the key div wont fade out

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-// When the key that is on the button is pressed, the corresponding sound is played.
-// When the key is pressed, the button also plays the animation.
 
-// Declare variables...Keys, Audio
+function removeTransition(e) {
+  // console.log(e);
+  if(e.propertyName !== 'transform') return;// skip it if it is not a transform property.
+  console.log(this);
+  e.target.classList.remove('playing');
+}
 
-// addEventListener for keydown on specific key
-window.addEventListener('keydown', keyPress)
-
-function keyPress(e) {
-// Detect whick key is pressed
+function playSound(e) {
+  // Detect whick key is pressed
 // console.log(e.keyCode);
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 // Check if there is an audio element for the key that was pressed
 // If there is not, then stlp this function from running until another key is pressed.
-if(!audio) {return};
-audio.play();
+if(!audio) return;
+key.classList.add('playing');
+audio.currentTime = 0;// rewind to the start of the sound file.
 // Play the corresponding audio of the pressed key.
-
-// add the class of 'playing' to the key div of the key that is being pressed.
-// key.classList.add('playing');
-
+audio.play();
+// setTimeout(function() {
+//   key.classList.remove('playing');}, 300);
 }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// addEventListener for keydown on specific key
+window.addEventListener('keydown', playSound);
+
+
+
+
+
 
