eecf96b56c7acb4ff8506c0451aa69b2a76c5332
Andrew TOwnsley <atownsley86@gmail.com>
2021-05-07 22:17:58 -0600

    updated comments

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ function removeTransition(e) {
 
 function playSound(e) {
   // Detect whick key is pressed
-// console.log(e.keyCode);
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-// Check if there is an audio element for the key that was pressed
-// If there is not, then stlp this function from running until another key is pressed.
-if(!audio) return;
-key.classList.add('playing');
-audio.currentTime = 0;// rewind to the start of the sound file.
-// Play the corresponding audio of the pressed key.
-audio.play();
-// setTimeout(function() {
-//   key.classList.remove('playing');}, 300);
+  // console.log(e.keyCode);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // Check if there is an audio element for the key that was pressed
+  // If there is not, then stlp this function from running until another key is pressed.
+  if(!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;// rewind to the start of the sound file.
+  // Play the corresponding audio of the pressed key.
+    audio.play();
+  // setTimeout(function() {
+  //   key.classList.remove('playing');}, 300);
 }
 
 const keys = document.querySelectorAll('.key');
